0e98188b2c8de569338ef71924a5bdd8998ed44d
mcdotjs <mirectm@gmail.com>
2025-02-22 09:50:38 +0100

    maze class

--- main.py
+++ main.py
@@ -2,23 +2,26 @@ from window import Window
 from line import Line
 from point import Point
 from cell import Cell
+from maze import Maze
 
 
 def main():
-    win = Window(800, 600)
-    p1 = Point(999, 100)
-    p2 = Point(199, 999)
-    cell = Cell(win, p1, p2)
-    p11 = Point(300, 10)
-    p22 = Point(10, 300)
-    cell1 = Cell(win, p11, p22)
+    win = Window(300, 300)
+    Maze(win=win)
+    # size = 33
+    # cols = 3
+    # rows = 10
+    # for j in range(cols):
+    #     for i in range(rows):
+    #         print(i)
+    #         c = j*size
+    #         t = i*size
+    #         cell = Cell(win,  Point(0+c, 0+t),  Point(size+c, size+t))
+    #         cell.draw()
 
-    cell.draw()
-    cell1.draw()
-    cell.draw_move(cell1)
-    line = Line(p1, p2)
-    win.draw_line(line, "white")
     win.wait_for_close()
+ #   line = Line(p1, p2)
+  #  win.draw_line(line, "white")
 
 
 main()
---
+++ maze.py
@@ -0,0 +1,48 @@
+from line import Line
+from point import Point
+from cell import Cell
+import time
+
+
+class Maze():
+    def __init__(
+        self,
+        x1=100,
+        y1=300,
+        num_rows=10,
+        num_cols=7,
+        cell_size_x=30,
+        cell_size_y=50,
+        win=None,
+    ):
+        self._win = win
+        self._x1 = x1
+        self._y1 = y1
+        self._cells = []
+        self._cols = num_cols
+        self._rows = num_rows
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._create_cells()
+
+        for col in range(len(self._cells)):
+            for cell in range(len(self._cells[col])):
+                self._draw_cell(col, cell)
+
+    def _create_cells(self):
+        for j in range(self._rows):
+            self._cells.append([])
+            for i in range(self._cols):
+                c = j*self._cell_size_x
+                t = i*self._cell_size_y
+                cell = Cell(self._win,  Point(self._x1+c, self._y1+t),
+                            Point(self._cell_size_x+c+self._x1, self._cell_size_y+t+self._y1))
+                self._cells[j].append(cell)
+
+    def _draw_cell(self, x, y):
+        self._cells[x][y].draw()
+        self._animate()
+
+    def _animate(self):
+        self._win.redraw()
+        time.sleep(0.05)
